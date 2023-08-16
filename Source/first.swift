d7026b6d911b27a88cd63535042388b93ae5c139
week <week@weekdeMac-mini.local>
2023-08-16 16:48:09 +0800

    project init

---
+++ Source/first.swift
@@ -0,0 +1,8 @@
+//
+//  first.swift
+//  MN5SwiftKit
+//
+//  Created by week on 2023/8/16.
+//
+
+import Foundation
---
+++ Tests/test.swift
@@ -0,0 +1,8 @@
+//
+//  test.swift
+//  MN5SwiftKit
+//
+//  Created by week on 2023/8/16.
+//
+
+import Foundation
